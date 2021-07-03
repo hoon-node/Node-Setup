33e0141601b3c8fdf9ee7a315b6ebaab6b3da8f8
hoon-node <86883770+hoon-node@users.noreply.github.com>
2021-07-03 18:19:17 +0200

    Update Step2_3_4.sh

--- Step2_3_4.sh
+++ Step2_3_4.sh
@@ -4,52 +4,52 @@
 
 run () {
    
-    echo "Enable IPv6 support for Docker"
-    echo ""
+    echo -e "\e[32mEnable IPv6 support for Docker"
+    echo -e "\e[39m"
     echo '{
     "ipv6": true,
     "fixed-cidr-v6": "2001:db8:1::/64"
 }' | sudo tee -a /etc/docker/daemon.json
 
     echo ""
-    echo "Restart the Docker process"
-    echo ""
+    echo -e "\e[32mRestart the Docker process"
+    echo -e "\e[39m"
     sudo systemctl restart docker
     
     echo ""
-    echo "Install iptables-persistent package"
-    echo ""
+    echo -e "\e[32mInstall iptables-persistent package"
+    echo -e "\e[39m"
     sudo apt-get install --yes iptables-persistent
     
     echo ""
-    echo "Enable NAT for the private Docker subnet on the host" 
-    echo ""
+    echo -e "\e[32mEnable NAT for the private Docker subnet on the host" 
+    echo -e "\e[39m"
     rule="POSTROUTING -s 2001:db8:1::/64 ! -o docker0 -j MASQUERADE" && \
 sudo ip6tables -t nat -C ${rule} || \
 sudo ip6tables -t nat -A ${rule} && \
 sudo sh -c "ip6tables-save > /etc/iptables/rules.v6"
     
     echo ""
-    echo "Install Git package" 
-    echo ""
+    echo -e "\e[32mInstall Git package" 
+    echo -e "\e[39m"
     sudo apt-get install --yes git
     
     echo ""
-    echo "Clone the GitHub repository"
-    echo ""
+    echo -e "\e[32mClone the GitHub repository"
+    echo -e "\e[39m"
     git clone https://github.com/sentinel-official/dvpn-node.git \
     ${HOME}/dvpn-node/
     
     echo ""
-    echo "Change the working directory and checkout to the latest tag"
-    echo ""
+    echo -e "\e[32mChange the working directory and checkout to the latest tag"
+    echo -e "\e[39m"
     cd ${HOME}/dvpn-node/ && \
 commit=$(git rev-list --tags --max-count=1) && \
 git checkout $(git describe --tags ${commit})
     
     echo ""
-    echo "Build the image"
-    echo ""
+    echo -e "\e[32mBuild the image"
+    echo -e "\e[39m"
     docker build --file Dockerfile \
     --tag sentinel-dvpn-node \
     --force-rm \
@@ -57,22 +57,22 @@ git checkout $(git describe --tags ${commit})
     --compress .
     
     echo ""
-    echo "Install openssl package"
-    echo ""
+    echo -e "\e[32mInstall openssl package"
+    echo -e "\e[39m"
     sudo apt-get install --yes openssl
     
    
     echo ""
-    echo "Initialize the application configuration"
-    echo ""
+    echo -e "\e[32mInitialize the application configuration"
+    echo -e "\e[39m"
     docker run --rm \
     --volume ${HOME}/.sentinelnode:/root/.sentinelnode \
     sentinel-dvpn-node process config init
    
    
     echo ""
-    echo "Initialize the application configuration"
-    echo ""
+    echo -e "\e[32mInitialize the wireguard configuration"
+    echo -e "\e[39m"
     docker run --rm \
     --volume ${HOME}/.sentinelnode:/root/.sentinelnode \
     sentinel-dvpn-node process wireguard config init
