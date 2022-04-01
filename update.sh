0d4f839738685a90b9941cb3d9d08540dac38a14
hoon-node <86883770+hoon-node@users.noreply.github.com>
2022-04-01 21:33:37 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+run () {
+        
+    echo -e "\e[32mRemove old Version"
+    echo -e "\e[39m"
+    rm -r ${HOME}/dvpn-node/
+
+    echo ""
+    echo -e "\e[32mClone Git"
+    echo -e "\e[39m"
+    git clone https://github.com/sentinel-official/dvpn-node.git
+    
+    echo ""
+    echo -e "\e[32mGot to new dir"
+    echo -e "\e[39m"
+    cd ${HOME}/dvpn-node/
+    
+    echo ""
+    echo -e "\e[32mFetch" 
+    echo -e "\e[39m"
+    git fetch
+    
+    echo ""
+    echo -e "\e[32mcheckout" 
+    echo -e "\e[39m"
+    git checkout v0.3.0
+    
+    echo ""
+    echo -e "\e[32mBuild docker"
+    echo -e "\e[39m"
+    docker build --file Dockerfile \
+    --tag sentinel-dvpn-node \
+    --force-rm \
+    --no-cache \
+    --compress .
+    
+}
+
+run
