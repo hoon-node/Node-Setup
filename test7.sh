d8a16322b29769730ee3e9627b3c026b1b037d41
hoon-node <86883770+hoon-node@users.noreply.github.com>
2022-06-15 00:19:22 +0200

    Update and rename test6.sh to test7.sh

--- test7.sh
+++ test7.sh
@@ -26,15 +26,15 @@ sed -i -e "s%\(remote_url *= *\).*%\1$remote_url%" ${HOME}/.sentinelnode/config.
 
 
 echo "\e[32mEnter node Moniker (the name your node is shown as):\e[m" 
-read moniker_temp
+read moniker_temp  </dev/tty
 
 
 echo "\e[32mEnter the price you want to charge in ___udvpn (1dvpn=1000000udvpn):\e[m" 
-read price_temp
+read price_temp  </dev/tty
 
 
 echo "\e[32mEnter your key name:\e[m" 
-read key_temp
+read key_temp  </dev/tty
 
 
 
