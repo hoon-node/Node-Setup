ca3cbb148417e0a9f5dac44dd0f920f81957f2b0
hoon-node <86883770+hoon-node@users.noreply.github.com>
2022-06-16 19:24:41 +0200

    Update Actual speedrun tho.sh

--- Actual speedrun tho.sh
+++ Actual speedrun tho.sh
@@ -35,27 +35,27 @@ run () {
     
     
     
-    us=$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd)
-    uss=( $us )
-    numb=$(echo -n "$us" | wc -w)
-    var=0
-    
-    while (( $var < $numb))
-    do
-        
-    echo ""
-    echo -e "\e[32mAdd user to Docker group" 
-    echo -e "\e[39m"
-    sudo usermod -aG docker ${uss[$var]}
-
-
-    echo ""
-    echo -e "\e[32mAdding...."
-    echo -e "\e[39m"
-    newgrp docker ${uss[$var]}
-    
-    var=$((var+1))
-    done
+#    us=$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd)
+#    uss=( $us )
+#    numb=$(echo -n "$us" | wc -w)
+#    var=0
+#    
+#    while (( $var < $numb))
+#    do
+#        
+#    echo ""
+#    echo -e "\e[32mAdd user to Docker group" 
+#    echo -e "\e[39m"
+#    sudo usermod -aG docker ${uss[$var]}
+#
+#
+#    echo ""
+#    echo -e "\e[32mAdding...."
+#    echo -e "\e[39m"
+#    newgrp docker ${uss[$var]}
+#    
+#    var=$((var+1))
+#    done
  
 
 #''''''''''''INSTALL DOCKER AND STUFF''''''''''''''''''''''''''''
