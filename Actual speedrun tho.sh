8cffec0956f09e0d91fde241e9be336e10fdd787
hoon-node <86883770+hoon-node@users.noreply.github.com>
2022-06-16 17:48:42 +0200

    Update Actual speedrun tho.sh

--- Actual speedrun tho.sh
+++ Actual speedrun tho.sh
@@ -201,7 +201,8 @@ read price_temp </dev/tty
 echo -e "\e[32mDo you want to import an existing wallet of yours? [y,n]\e[m"
 read input </dev/tty
 if [[ $input == "Y" || $input == "y" || $input == "yes" || $input == "Yes" || $input == "zes" || $input == "Zes" || $input == "z" || $input == "Z" ]]; then
-        echo "Ok, import your key after the script is finished and use the same keyname you enter in the next prompt"
+        echo ""
+        echo "\e[32mOk, import your key after the script is finished and use the same keyname you enter in the next prompt\e[m"
 sleep 10
 echo ""
 echo -e "\e[32mEnter your key name:\e[m" 
