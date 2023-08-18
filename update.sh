3d8864ed3f6580156a4274360c82008730c007f6
hoon-node <86883770+hoon-node@users.noreply.github.com>
2023-08-18 23:07:06 +0200

    Update update.sh
    0.7.0

--- update.sh
+++ update.sh
@@ -9,41 +9,55 @@ run () {
             
     echo -e "\e[32mRemove Node Docker"
     echo -e "\e[39m"
-    sudo docker rm -f sentinel-dvpn-node     
-        
-    
-    echo -e "\e[32mRemove old Version"
-    echo -e "\e[39m"
-    rm -r ${HOME}/dvpn-node/
+    sudo docker rm -f sentinel-dvpn-node 
+
 
     echo ""
-    echo -e "\e[32mClone Git"
+    echo -e "\e[32mClone repository..."
     echo -e "\e[39m"
+
     git clone https://github.com/sentinel-official/dvpn-node.git
-    
-    echo ""
-    echo -e "\e[32mGot to new dir"
-    echo -e "\e[39m"
     cd ${HOME}/dvpn-node/
-    
+
     echo ""
-    echo -e "\e[32mFetch" 
+    echo -e "\e[32mSwitching to version v0.7.0..."
     echo -e "\e[39m"
+
     git fetch
-    
+    git checkout v0.7.0
+
     echo ""
-    echo -e "\e[32mcheckout" 
+    echo -e "\e[32mBuilding Docker image..."
     echo -e "\e[39m"
-    git checkout v0.3.2
-    
-    echo ""
-    echo -e "\e[32mBuild docker"
-    echo -e "\e[39m"
+
     docker build --file Dockerfile \
-    --tag sentinel-dvpn-node \
-    --force-rm \
-    --no-cache \
-    --compress .
+        --tag sentinel-dvpn-node \
+        --force-rm \
+        --no-cache \
+        --compress .
+
+    echo ""
+    echo -e "\e[32mUpdating configuration file..."
+    echo -e "\e[39m"
+
+    # Update configuration file
+    sed -i 's/price = /gigabyte_prices = /' ~/.sentinelnode/config.toml
+    sed -i '/gigabyte_prices = /a hourly_prices = "4900000udvpn"' ~/.sentinelnode/config.toml
+
+
+     echo ""
+    echo -e "\e[32mCleaning up Docker builder cache..."
+    echo -e "\e[39m"
+
+    # Automatically confirm Docker builder cache pruning
+    yes | docker builder prune
+    
+
+    echo ""
+    echo -e "\e[32mUpdate completed successfully!"
+    echo -e "\e[39m"
+
+
     
 }
 
