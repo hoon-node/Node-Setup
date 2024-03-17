33263165807da0c0de017a8c88bb13bc4e6dca27
hoon-node <86883770+hoon-node@users.noreply.github.com>
2024-03-17 18:13:51 +0100

    Create restartV2rayWireguard.sh

---
+++ sh/restartV2rayWireguard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Read the node type from the config file
+node_type=$(cat /root/.sentinelnode/config.toml | grep type | awk -F'"' '{print $2}')
+
+if [ "$node_type" == "v2ray" ]; then
+  echo "Restarting v2ray node..."
+  sudo docker rm -f sentinel-dvpn-node && echo "Docker container removed" && \
+  rm ${HOME}/.sentinelnode/data.db && echo "File deleted" && \
+  sleep 2 && echo "Paused for 4 seconds" && \
+  sudo docker run -d \
+    --name sentinel-dvpn-node \
+    --restart unless-stopped \
+    --volume /root/.sentinelnode:/root/.sentinelnode \
+    --publish $(grep -oP 'listen_on = \"0.0.0.0:\K\d+' ~/.sentinelnode/config.toml):$(grep -oP 'listen_on = \"0.0.0.0:\K\d+' ~/.sentinelnode/config.toml)/tcp \
+    --publish $(grep -oP 'listen_port = \K\d+' ~/.sentinelnode/v2ray.toml):$(grep -oP 'listen_port = \K\d+' ~/.sentinelnode/v2ray.toml)/tcp \
+    sentinel-dvpn-node process start
+elif [ "$node_type" == "wireguard" ]; then
+  echo "Restarting wireguard node..."
+  sudo docker rm -f sentinel-dvpn-node && echo "Docker container removed" && \
+  rm ${HOME}/.sentinelnode/data.db && echo "File deleted" && \
+  sleep 2 && echo "Paused for 4 seconds" && \
+  sudo docker run -d --name sentinel-dvpn-node --restart unless-stopped --volume /root/.sentinelnode:/root/.sentinelnode --volume /lib/modules:/lib/modules --cap-drop ALL --cap-add NET_ADMIN --cap-add NET_BIND_SERVICE --cap-add NET_RAW --cap-add SYS_MODULE --sysctl net.ipv4.ip_forward=1 --sysctl net.ipv6.conf.all.disable_ipv6=0 --sysctl net.ipv6.conf.all.forwarding=1 --sysctl net.ipv6.conf.default.forwarding=1 --publish $(grep -oP 'listen_on = \"0.0.0.0:\K\d+' ~/.sentinelnode/config.toml):$(grep -oP 'listen_on = \"0.0.0.0:\K\d+' ~/.sentinelnode/config.toml)/tcp --publish $(grep -oP 'listen_port = \K\d+' ~/.sentinelnode/wireguard.toml):$(grep -oP 'listen_port = \K\d+' ~/.sentinelnode/wireguard.toml)/udp sentinel-dvpn-node process start
+else
+  echo "Unknown node type: $node_type"
+  exit 1
+fi
+
+echo "Node restart completed successfully."
